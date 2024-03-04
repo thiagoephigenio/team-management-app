1c8a947f267e9ed03772b622de15ee4e5441f9e5
thiagoephigenio <thiago.costa@medcloud.co>
2024-03-04 17:43:44 -0300

    feat(frontend): change css styles

--- frontend/src/presentation/app.styles.ts
+++ frontend/src/presentation/app.styles.ts
@@ -17,6 +17,22 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  *::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    background-color: #d4d4d8;
+  }
+  *::-webkit-scrollbar-thumb {
+    border-radius:5px;
+    background: #9ca3af;
+  }
+  *::-webkit-scrollbar-thumb:hover {
+    background-color: #6b7280;
+    cursor: pointer;
+    filter: brightness(0.8);
+  }
+
+
   button, a {
     border: 0;
     text-decoration: none;
@@ -27,8 +43,9 @@ export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     display: flex;
-    width: 100vw;
-    min-height: 100vh;
+    max-width: 100vw;
+    max-height: 100vh;
+    overflow: auto;
   }
 
   #root {
